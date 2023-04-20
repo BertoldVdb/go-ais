df0b9464d3479f8195578eccf6b190aa653cc5d2
Ersin Buckley <ersinbuckley@gmail.com>
2023-04-20 17:29:24 +1200

    add a soak test. read the same archive from the fast parser and slow parser and assert that they have the same content

--- bench_decode_test.go
+++ bench_decode_test.go
@@ -1,9 +1,17 @@
-package ais
+package ais_test
 
-import "testing"
+import (
+	"bufio"
+	"compress/gzip"
+	"github.com/BertoldVdb/go-ais"
+	"github.com/BertoldVdb/go-ais/aisnmea"
+	"io"
+	"os"
+	"testing"
+)
 
 func runTest(b *testing.B, parseFast bool) {
-	x := CodecNew(false, false, parseFast)
+	x := ais.CodecNew(false, false, parseFast)
 	line := "000001000011100000000111101001010010000000000000000000000000011111110111110111001101001000100000111001110001011110001000101011000101000111011110000000001110101000001100"
 	source := []byte(line)
 	/* Convert ascii '0' and '1' to real 0 and 1 */
@@ -20,5 +28,48 @@ func BenchmarkDecode(b *testing.B) {
 	})
 	b.Run("do not prefer reflection", func(b *testing.B) {
 		runTest(b, true)
+	})
+}
+
+func readFileTest(b *testing.B, benchFile string, parseFast bool) {
+	c := ais.CodecNew(false, false, parseFast)
+	nm := aisnmea.NMEACodecNew(c)
+	var reader io.Reader
+	fp, err := os.Open(benchFile)
+	if err != nil {
+		b.Fatal("could not open ", benchFile, "\nerr:\n", err)
+	}
+	stat, err := fp.Stat()
+	b.SetBytes(stat.Size())
+	defer fp.Close()
+	reader, err = gzip.NewReader(fp)
+	if err != nil {
+		b.Fatal("could not create gzip reader", err)
+	}
+
+	// create line by line scanner, read each line and decode it
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		_, err := nm.ParseSentence(string(line))
+		if err != nil {
+			return
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		b.Fatal("error reading file", err)
+	}
+
+}
+func BenchmarkDecodeFile(b *testing.B) {
+	testFile := os.Getenv("NMEA_BENCH_FILE")
+	if testFile == "" {
+		b.Skip("NMEA_BENCH_FILE not set")
+	}
+	b.Run("prefer reflection", func(b *testing.B) {
+		readFileTest(b, testFile, false)
+	})
+	b.Run("do not prefer reflection", func(b *testing.B) {
+		readFileTest(b, testFile, true)
 	})
 }
